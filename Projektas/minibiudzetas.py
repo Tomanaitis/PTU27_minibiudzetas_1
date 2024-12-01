7ebfdad2efb5921aaba9aa2464212357e56ea989
TomasSimonaitis <boombox1x@gmail.com>
2024-12-01 12:40:25 +0200

    Patobulinta meniu ir laiko pasirinkimas
    sukurta vidinė while funckija laiko pasirinkime.
    prideta galimybė pasirinkti dabartinį laiką arba savo įvestą laiką.
    padaryta, kad galimi pasirinkimai TIK iš meniu.

--- Projektas/minibiudzetas.py
+++ Projektas/minibiudzetas.py
@@ -7,6 +7,7 @@
 # 5. Atspausdinti statistiką
 # q - Išeiti
 
+
 # Vartotojui leidžiam įvesti tokius duomenis - DATA(datetime arba tiesiog stringas), pajamų ar išlaidų
 # PAVADINIMA(pvz. pajamose - avansas, atlyginimas, stipendija ar pan, išlaidose - maistas, įvairūs pirkiniai,
 # būsto išlaidos ir tt) ir SUMA (SKAICIUS).
@@ -19,13 +20,28 @@
 
 # Reikėtų panaudoti bent vieną savo parašytą funkciją iškeltą į kitą failą ir importuojamą į pagrindinę programą.
 #
-# Padarę šį, pradinį variantą, prijungiam trynimo funkciją ir paieškos funkciją, loginimą. Trinti per indeksą,
+# Padarę šį, pradinį variantą, prijungiam trynimo funkciją ir paieškos funkciją, loginimą # LOGGINIMAS YRA P15 logging.
+# Trinti per indeksą,
 # pradžioje išvedus turimus duomenis su indekso numeriu, tam galime panaudoti ENUMERATE. Pabandykim
 # bent dalį veiksmų kelti į funkcijas.
-import pickle
+import datetime
 
+# import pickle
+
+# with open("pajamos.pickle", mode="wb") as file:
+#     # noinspection PyTypeChecker
+#     pickle.dump(pajamos, file)
+#
+# with open("islaidos.pickle", mode="rb") as file:
+#     # noinspection PyTypeChecker
+#     pickle.dump(islaidos, file)
+
+
+# biudzetas = [pajamos, islaidos] picklui uzkrauti
 pajamos = []
 islaidos = []
+meniu_pasirinkimai = ["1", "2", "3", "4", "5", "6", "7", "stop"]
+
 while True:
     print("1. Įvesti pajamas \n"
           "2. Įvesti išlaidas\n"
@@ -35,12 +51,31 @@ while True:
           "6. trynimo funkciją\n"
           "7. paieškos funkciją\n"
           "stop. baigti programą")
-    pasirinkimas = input("pasirinkimas:   ")
+    pasirinkimas = input("pasirinkimas: ")
+    data = None
+    if pasirinkimas not in meniu_pasirinkimai:
+        print("pasirinkimas neegzistuoja, bandykite dar kartą ")
+        continue
     if pasirinkimas == "stop":
         print("Programa baigta")
         break
     if pasirinkimas == "1":
-        data = input("įveskite datą yyyy-mm-dd: ")
+        while True:
+            print("1. įvesti savo laiką\n"
+                  "2. įvesti dabartinį laiką")
+            laiko_pasirinkimas = input("Laiko įvedimo pasirinkimas: ")
+            if laiko_pasirinkimas == "1":
+                data = input("įveskite datą yyyy-mm-dd: ")
+                print(f"Įvestas laikas: {data}")
+                break
+            if laiko_pasirinkimas == "2":
+                dabartinis_laikas = datetime.datetime.now()
+                data = dabartinis_laikas.strftime("%Y-%m-%d %H:%M:%S")
+                print(f"dabartinis laikas: {data}")
+                break
+            else:
+                print(f"pasirinkimas neegzistuoja, bandykite išnaujo")
+                continue
         pavadinimas = input("įveskite pajamų pavadinimą: ")
         suma = float(input("įveskite pajamų sumą: "))
         pajamos.append([data, pavadinimas, suma])
@@ -65,18 +100,4 @@ while True:
         print(f"visos išlaidos: {islaidu_suma} EUR")
         print(f"balancas; {balancas} EUR")
     # if pasirinkimas == "6":
-
-filas = open("logginimas.txt", mode="w", encoding="utf-8")
-filas.write(f"Visos įrasšytos pajamos: {pajamos}\n")
-filas.write(f"Visos įrašytos išlaidos: {islaidos}\n")
-# file.write(f"Pajamų suma: {pajamu_suma} EUR")
-# file.write(f"Išlaidų suma: {islaidu_suma} EUR")
-# file.write(f"balancas: {balancas} EUR")
-
-with open("pajamos.pickle", mode="wb") as file:
-    # noinspection PyTypeChecker
-    pickle.dump(pajamos, file)
-
-with open("islaidos.pickle", mode="rb") as file:
-    # noinspection PyTypeChecker
-    pickle.dump(islaidos, file)
+# LOGGINIMAS YRA P15 logging
